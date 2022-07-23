9009be853031717e1f39d0ac20ea2c78958decd9
Nirflector <nirflector@gmail.com>
2022-07-23 18:13:12 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to C++ Playground!";
+    cout << "My personal testing application for C++ stuff.";
+    return 0;
+}
+
+//--------------------------------------------------------------------------------------------------------------------//
+// Section 1: Basic Concepts
+//--------------------------------------------------------------------------------------------------------------------//
+
+// Every program has an entry method called "main"
+
