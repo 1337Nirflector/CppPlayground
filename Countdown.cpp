ffc3f51a6fc69bf8477ff50fe6a59ac50d061bd8
Nirflector <nirflector@gmail.com>
2022-07-24 11:08:00 +0200

    Finished first two sections and challenges

---
+++ Countdown.cpp
@@ -0,0 +1,15 @@
+#include <iostream>;
+using namespace std;
+
+int countdown() {
+    int n;
+    cin >> n;
+    while(n>=1)
+    {
+        cout<<n<<endl;
+        if(n%5==0)
+            cout<<"Beep"<<endl;
+        n--;
+    }
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -11,5 +11,63 @@ int main() {
 // Section 1: Basic Concepts
 //--------------------------------------------------------------------------------------------------------------------//
 
+/**
+ * ● C++ contains operator precedence: Multiplication is done before addition and calculations in parentheses are done first
+ * ● C++ is case sensitive
+ */
+
 // Every program has an entry method called "main"
+
+/**
+* using namespace std;
+ *
+ * A namespace is a declarative region that provides a scope to the names of the elements inside it.
+ * The std namespace includes feature of the C++ Standard Library.
+*/
+
+// Line breaks are made with the "endl" keyword or \n inside the String.
+int sect1a() {
+    cout << "Hold on! There comes a line break." << endl;
+    cout << "Woosh!";
+}
+
+// Variables
+int myVar = 10;
+
+void sect1b() {
+    cout << myVar;
+}
+
+// Input
+/**
+* cout << "Enter a number\n";
+ * cin >> num
+*/
+
+// Assignment and Increment
+int x = 10;
+
+void sect1c() {
+    x += x + 4; // equivalent to x = x + 4
+}
+
+void sect1d() {
+    x++; // equivalent to x = x + 1
+}
+
+//--------------------------------------------------------------------------------------------------------------------//
+// Section 2: Conditionals and Loops
+//--------------------------------------------------------------------------------------------------------------------//
+
+// Nothing new.
+
+//--------------------------------------------------------------------------------------------------------------------//
+// Section 3: Data Types, Arrays, Pointers
+//--------------------------------------------------------------------------------------------------------------------//
+
+// A signed integer can hold negative and positive numbers.
+signed int a;
+
+// An unsigned integer can hold only positive values.
+unsigned int b;
 
